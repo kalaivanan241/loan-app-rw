2bd31249ee2969492c1c9991b914db3992bdd0bd
kalai241 <kalaivanan.gunasekaran@privetechnologies.com>
2023-06-16 11:28:22 +0800

    Updated to use tailwind and shacdn

--- web/src/layouts/GeneralLayout/GeneralLayout.tsx
+++ web/src/layouts/GeneralLayout/GeneralLayout.tsx
@@ -1,31 +1,48 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from 'src/components/ui/navigation-menu'
+
 type GeneralLayoutProps = {
   children?: React.ReactNode
-};
+}
 
 const GeneralLayout = ({ children }: GeneralLayoutProps) => {
   return (
     <>
       <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-            <li>
-              <Link to={routes.loans()}>Loans</Link>
-            </li>
-            <li>
-              <Link to={routes.debts()}>Debts</Link>
-            </li>
-          </ul>
-        </nav>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link to={routes.home()}>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to={routes.loans()}>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Loans
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to={routes.debts()}>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Debts
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </header>
-      <main>
-       {children}
-      </main>
+      <main>{children}</main>
     </>
   )
 }
